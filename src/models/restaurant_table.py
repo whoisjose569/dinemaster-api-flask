9e90869f722904b643b49b1b598058c5342c3fac
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-12-13 17:10:37 -0300

    chore: adjust on table_status default valor

--- src/models/restaurant_table.py
+++ src/models/restaurant_table.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 class RestaurantTable(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True, autoincrement=True)
     table_number: Mapped[int] = mapped_column(sa.Integer, unique=True)
-    table_status: Mapped[str] = mapped_column(sa.String)
+    table_status: Mapped[str] = mapped_column(sa.String(20), default="livre")
     
     
