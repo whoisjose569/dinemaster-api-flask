a59cb11b4c0418ce571be390a2c35415ada4c4b7
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-12-16 16:53:04 -0300

    feat: add custom error for check table number exists method

--- src/repositories/restaurant_table/restaurant_table_repository.py
+++ src/repositories/restaurant_table/restaurant_table_repository.py
@@ -1,8 +1,11 @@
 from src.models.restaurant_table import RestaurantTable
 from src.models.base import db
+from src.errors.custom_errors import TableAlreadyExistsError
 
 def _check_table_number_exists(data):
-    return db.session.query(RestaurantTable).filter_by(table_number = data["table_number"]).first()
+    table_on_db = db.session.query(RestaurantTable).filter_by(table_number = data["table_number"]).first()
+    if table_on_db:
+        raise TableAlreadyExistsError(data["table_number"])
 
 def _create_restaurant_table(data):
     restaurant_table = RestaurantTable(table_number=data['table_number'])
