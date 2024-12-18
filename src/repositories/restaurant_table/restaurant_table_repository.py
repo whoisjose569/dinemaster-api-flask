81cd33facbe92b650bdd313eac99b29984024dba
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-12-18 16:58:51 -0300

    refact: refactoring to adjust best practices

--- src/repositories/restaurant_table/restaurant_table_repository.py
+++ src/repositories/restaurant_table/restaurant_table_repository.py
@@ -2,15 +2,15 @@ from src.models.restaurant_table import RestaurantTable
 from src.models.base import db
 from src.errors.custom_errors import TableAlreadyExistsError
 
-def _check_table_number_exists(data):
-    table_on_db = db.session.query(RestaurantTable).filter_by(table_number = data["table_number"]).first()
-    if table_on_db:
-        raise TableAlreadyExistsError(data["table_number"])
-
-def _create_restaurant_table(data):
-    restaurant_table = RestaurantTable(table_number=data['table_number'])
-    db.session.add(restaurant_table)
-    db.session.commit()
-    return restaurant_table
-
+class RestaurantTableRepository:
+    def __init__(self):
+        self.__session = db.session
     
+    def check_table_number_exists(self, data):
+        return self.__session.query(RestaurantTable).filter_by(table_number = data["table_number"]).first()
+
+    def create_restaurant_table(self, data):
+        restaurant_table = RestaurantTable(table_number=data['table_number'])
+        self.__session.add(restaurant_table)
+        self.__session.commit()
+        return restaurant_table
