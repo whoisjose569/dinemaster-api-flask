9146050d7d08efc685fe044d7a6b4eea6abfdb23
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-12-18 20:51:00 -0300

    feat: start implementing schema validators

---
+++ src/schemas/restaurant_table_schemas/restaurant_table_schemas.py
@@ -0,0 +1,22 @@
+from marshmallow import fields, validates
+from src.models.restaurant_table import RestaurantTable
+from src.errors.custom_errors import TableNumberValidationError, TableNumberMustBeANumber
+from src.utils import ma
+
+class CreateRestaurantTableSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = RestaurantTable
+        exclude = ("id",)
+    
+    table_status = fields.String(missing=None)
+    table_number = fields.Raw(required=True, strict=True)
+    
+    @validates("table_number")
+    def validate_table_number(self, value):
+        if not isinstance(value, int):
+            raise TableNumberMustBeANumber("Table number must be a integer")
+        if value <= 0:
+            raise TableNumberValidationError("Table number must be positive")
+    
+    
+    
