d45495708d56a8a324af7d5f900faee7dd700110
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-06 09:03:27 -0300

    tests: implementing domain create_table test

---
+++ tests/unit/test_domain/test_restaurant_table_use_cases.py
@@ -0,0 +1,27 @@
+import pytest
+from src.domain.restaurant_table_use_cases.create_table import CreateRestaurantTableService
+from src.errors.custom_errors import TableAlreadyExistsError
+
+
+def test_create_restaurant_table_raises_error_when_table_exists(mocker):
+    mock_repo = mocker.Mock()
+    mock_repo.check_table_number_exists.return_value = True
+    service = CreateRestaurantTableService(mock_repo)
+    
+    with pytest.raises(TableAlreadyExistsError):
+        service.create_restaurant_table({"table_number": 1})
+    
+    mock_repo.check_table_number_exists.assert_called_once_with({"table_number": 1})
+
+def test_create_restaurant_table_when_table_not_exists(mocker):
+    mock_repo = mocker.Mock()
+    mock_repo.check_table_number_exists.return_value = False
+    service = CreateRestaurantTableService(mock_repo)
+    
+    data = {"table_number": 1}
+    
+    service.create_restaurant_table(data)
+    
+    mock_repo.create_restaurant_table.assert_called_once_with(data)
+    mock_repo.check_table_number_exists.assert_called_once_with(data)
+    
