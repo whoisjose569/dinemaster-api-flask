7b71c06e3fd06b0d802f983c7ae6285a83f5eb3e
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-08 16:43:29 -0300

    feat: implementing route for list_table_by_number on restaraunt_table controller

--- src/controllers/restaurant_table/restaurant_table_controllers.py
+++ src/controllers/restaurant_table/restaurant_table_controllers.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request
 from http import HTTPStatus
 from src.composer.restaurant_table.create_table_composer import create_table_composer
-from src.composer.restaurant_table_presenter_composer import create_restaurant_table_presenter_composer
+from src.composer.restaurant_table.list_table_composer import list_table_composer
+from src.composer.restaurant_table_presenter_composer import restaurant_table_presenter_composer
 from src.schemas.restaurant_table_schemas.restaurant_table_schemas import CreateRestaurantTableSchema
+
 
 bp = Blueprint('restaurant_table', __name__, url_prefix='/tables')
 
@@ -16,8 +18,20 @@ def create_restaurant_table():
         use_case = create_table_composer()
         response = use_case.create_restaurant_table(validated_data)
         
-        presenter = create_restaurant_table_presenter_composer()
+        presenter = restaurant_table_presenter_composer()
         formatted_response = presenter.format_table_presenter(response)
         return formatted_response, HTTPStatus.CREATED
+    except Exception as exc:
+        raise
+
+@bp.route('/<int:table_number>', methods=["GET"])
+def list_restaurant_table_by_number(table_number):
+    try:
+        use_case = list_table_composer()
+        response = use_case.list_table_by_number(table_number)
+        
+        presenter = restaurant_table_presenter_composer()
+        formatted_response = presenter.format_table_presenter(response)
+        return formatted_response, HTTPStatus.OK
     except Exception as exc:
         raise
