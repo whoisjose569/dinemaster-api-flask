e4b3f4dda39831c416a2a5a61a8ac5ac0f634590
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-08 16:41:58 -0300

    fix: adjusts some names for project quality

--- src/presenters/restaurant_table/restaurant_table_presenter.py
+++ src/presenters/restaurant_table/restaurant_table_presenter.py
@@ -1,7 +1,6 @@
 class RestaurantTablePresenter:
     def format_table_presenter(self, data):
         return{
-            "msg": 'RestaurantTable created!.',
             "id": data.id,
             "table_number": data.table_number,
             "table_status": data.table_status
