fbfede47dd3d25bf38dcbf15934e2b389fe8b95a
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-13 09:19:23 -0300

    tests: implementing integration tests for list_table_by_number controller

--- tests/integration/controllers/test_restaurant_table_controllers.py
+++ tests/integration/controllers/test_restaurant_table_controllers.py
@@ -19,14 +19,11 @@ def test_create_table(client, mocker, db_session):
     mock_presenter.format_table_presenter.return_value = {"table_number": 1}
     
     response = client.post('/tables/', json=data)
-
-    print(response.json)
-    
+ 
     assert response.status_code == HTTPStatus.CREATED
     
     assert response.json == {
             "id": 1,
-            "msg": 'RestaurantTable created!.',
             "table_number": 1,
             "table_status": "livre"
         }
@@ -35,7 +32,35 @@ def test_create_table(client, mocker, db_session):
     db_session.delete(table_in_db)
     db_session.commit()
     
+def test_list_table_by_number(client, mocker, db_session):
+    mock_list_table_composer = mocker.patch('src.composer.restaurant_table.list_table_composer')
+    mock_service = mocker.Mock()
+    mock_list_table_composer.return_value = mock_service
+    
+    mock_presenter_composer = mocker.patch('src.composer.restaurant_table_presenter_composer')
+    mock_presenter = mocker.Mock()
+    mock_presenter_composer.return_value = mock_presenter
+    
+    table = RestaurantTable(table_number = 1)
+    db_session.add(table)
+    db_session.commit()
+    
+    data = {"id": 1, "table_number": 1, "table_status": "livre"}
+    
+    mock_service.list_table_by_number.return_value = data
+    mock_presenter.format_table_presenter.return_value = data
+    
+    response = client.get("/tables/1")
 
+    assert response.status_code == 200
+    assert response.json == {
+            "id": 1,
+            "table_number": 1,
+            "table_status": "livre"
+        }
+    
+    db_session.delete(table)
+    db_session.commit()
     
     
     
