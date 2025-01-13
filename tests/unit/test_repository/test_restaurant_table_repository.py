cd2a6a07d978e5c08a53a4192b0d3b8e0aa20465
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-13 14:33:04 -0300

    tests: implementing test_list_all_tables tests

--- tests/unit/test_repository/test_restaurant_table_repository.py
+++ tests/unit/test_repository/test_restaurant_table_repository.py
@@ -30,3 +30,31 @@ def test_list_table_by_number(db_session):
     
     db_session.delete(existing_table)
     db_session.commit()
+
+def test_list_all_tables(db_session):
+    existing_table_one = RestaurantTable(table_number = 1)
+    db_session.add(existing_table_one)
+    db_session.commit()
+    
+    existing_table_two = RestaurantTable(table_number = 2)
+    db_session.add(existing_table_two)
+    db_session.commit()
+    
+    repository = RestaurantTableRepository()
+    
+    result = repository.list_restaurant_table()
+    
+    assert result is not None
+    assert len(result) == 2
+    assert result[0].id == existing_table_one.id
+    assert result[0].table_number == existing_table_one.table_number
+    assert result[0].table_status == existing_table_one.table_status
+    assert result[1].id == existing_table_two.id
+    assert result[1].table_number == existing_table_two.table_number
+    assert result[1].table_status == existing_table_two.table_status
+    
+    db_session.delete(existing_table_one)
+    db_session.commit()
+    
+    db_session.delete(existing_table_two)
+    db_session.commit()
