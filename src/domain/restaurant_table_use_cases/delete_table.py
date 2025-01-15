ae813d31b504ba663db591c39ff312f36a9523a0
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-15 09:43:30 -0300

    chore: adjusts on delete_table service on domain

--- src/domain/restaurant_table_use_cases/delete_table.py
+++ src/domain/restaurant_table_use_cases/delete_table.py
@@ -7,6 +7,7 @@ class DeleteRestaurantTableService:
         self.__restaurant_table_repository = restaurant_table_repository
     
     def delete_table(self, table_number):
-        if not self.__restaurant_table_repository.check_table_number_exists(table_number):
+        if not self.__restaurant_table_repository.check_table_exists(table_number):
             raise TableNotExists(table_number)
-        return self.__restaurant_table_repository.delete_restaurant_table(table_number)
+        self.__restaurant_table_repository.delete_restaurant_table(table_number)
+        return None
