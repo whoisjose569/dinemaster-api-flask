a83339b823167da12cc4e979647c78fa5869f554
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-15 09:13:04 -0300

    feat: implementing delete_table controller

--- src/controllers/restaurant_table/restaurant_table_controllers.py
+++ src/controllers/restaurant_table/restaurant_table_controllers.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from src.composer.restaurant_table.create_table_composer import create_table_composer
 from src.composer.restaurant_table.list_table_composer import list_table_composer
 from src.composer.restaurant_table.list_all_table_composer import list_all_table_composer
+from src.composer.restaurant_table.delete_table_composer import delete_table_composer
 from src.composer.restaurant_table_presenter_composer import restaurant_table_presenter_composer
 from src.schemas.restaurant_table_schemas.restaurant_table_schemas import CreateRestaurantTableSchema
 
@@ -46,5 +47,15 @@ def list_all_restaurant_table():
         presenter = restaurant_table_presenter_composer()
         formatted_response = presenter.format_all_table_presenter(response)
         return formatted_response, HTTPStatus.OK
+    except Exception as exc:
+        raise
+
+@bp.route('/<int:table_number', methods=["DELETE"])
+def delete_restaurant_table(table_number):
+    try:
+        use_case = delete_table_composer()
+        response = use_case.delete_table(table_number)
+        
+        return None, HTTPStatus.NO_CONTENT
     except Exception as exc:
         raise
