adc6cf339000aac66a0b1d4094dee468d2ab68d4
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-20 16:00:41 -0300

    feat: implementing UpdateTableSchema on schemas

--- src/schemas/restaurant_table_schemas/restaurant_table_schemas.py
+++ src/schemas/restaurant_table_schemas/restaurant_table_schemas.py
@@ -1,6 +1,6 @@
 from marshmallow import fields, validates
 from src.models.restaurant_table import RestaurantTable
-from src.errors.custom_errors import TableNumberValidationError, TableNumberMustBeANumber
+from src.errors.custom_errors import TableNumberValidationError, TableNumberMustBeANumber, TableStatusMustBeAString
 from src.utils import ma
 
 class CreateRestaurantTableSchema(ma.SQLAlchemyAutoSchema):
@@ -17,6 +17,18 @@ class CreateRestaurantTableSchema(ma.SQLAlchemyAutoSchema):
             raise TableNumberMustBeANumber("Table number must be an integer")
         if value <= 0:
             raise TableNumberValidationError("Table number must be positive")
+
+class UpdateRestaurantTableSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = RestaurantTable
+        exclude = ("id", "table_number",)
+    
+    table_status = fields.Raw(required=True)
+    
+    @validates("table_status")
+    def validate_table_status(self, value):
+        if not isinstance(value, str):
+            raise TableStatusMustBeAString("Table number must be an String")
     
     
     
