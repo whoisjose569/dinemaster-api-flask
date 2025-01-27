462261f73c3c2d5e0eb823b5f3b13830c1b41f83
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-27 08:40:23 -0300

    chore: add table name to model restaurant table

--- src/models/restaurant_table.py
+++ src/models/restaurant_table.py
@@ -3,6 +3,8 @@ from .base import db
 from sqlalchemy.orm import Mapped, mapped_column
 
 class RestaurantTable(db.Model):
+    __tablename__ = "restaurant_table"
+    
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True, autoincrement=True)
     table_number: Mapped[int] = mapped_column(sa.Integer, unique=True)
     table_status: Mapped[str] = mapped_column(sa.String(20), default="livre")
