2813c3f0fedc1fe3bfcce44610f743e32074dca9
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-28 09:41:31 -0300

    feat: implementing check_table_number_exists on order repository

--- src/repositories/orders/orders_repository.py
+++ src/repositories/orders/orders_repository.py
@@ -1,9 +1,13 @@
+from src.models.restaurant_table import RestaurantTable
 from src.models.orders import Orders
 from src.models.base import db
 
 class OrdersRepository:
     def __init__(self):
         self.__session = db.session
+
+    def check_table_exists(self, data):
+        return self.__session.query(RestaurantTable).filter_by(id = data["restaurant_table_id"]).first()
     
     def create_order(self, data):
         new_order = Orders(**data)
