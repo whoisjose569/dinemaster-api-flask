a311dfc3f3786cd33779b5f5999218127a582f81
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-28 09:42:30 -0300

    feat: implementing schema on order controller

--- src/controllers/orders/orders_controllers.py
+++ src/controllers/orders/orders_controllers.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from http import HTTPStatus
+from src.schemas.order_schemas.order_schemas import CreateOrderSchema
 from src.composer.orders_presenter_composer import order_presenter_composer
 from src.composer.orders.create_order_composer import create_order_composer
 
@@ -9,8 +10,12 @@ bp = Blueprint('orders',__name__,url_prefix="/orders")
 def create_order():
     try:
         data = request.json
+        schema = CreateOrderSchema()
+        
+        validated_data = schema.load(data)
+        
         use_case = create_order_composer()
-        response = use_case.create_order(data)
+        response = use_case.create_order(validated_data)
 
         presenter = order_presenter_composer()
         formatted_response = presenter.format_order_presenter(response)
