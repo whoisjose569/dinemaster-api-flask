8dc747786f85b9d52c1fcdc9a8f9a7513bc8f3a3
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-28 09:40:20 -0300

    feat: implementing CreateOrderSchema for validating

---
+++ src/schemas/order_schemas/order_schemas.py
@@ -0,0 +1,18 @@
+from marshmallow import fields, validates
+from src.models.orders import Orders
+from src.errors.custom_errors import OrderStatusMustBeAString
+from src.utils import ma
+
+class CreateOrderSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Orders
+        exclude = ("id",)
+    
+    restaurant_table_id = fields.Integer(load_default=None)
+    order_status = fields.Raw(load_default="Em preparo")
+    delivery_type = fields.String(load_default=None)
+    
+    @validates("order_status")
+    def validate_order_status(self, value):
+        if not isinstance(value, str):
+            raise OrderStatusMustBeAString("Order status must be an String")
