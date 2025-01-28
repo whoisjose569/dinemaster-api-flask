97f0b22d41ff1c49275e707b2641adef85fe5fd3
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-01-28 09:41:58 -0300

    feat: implementing validating on CreateOrderService

--- src/domain/orders_use_cases/create_order.py
+++ src/domain/orders_use_cases/create_order.py
@@ -1,9 +1,13 @@
 from src.repositories.orders.orders_repository import OrdersRepository
+from src.errors.custom_errors import TableNotExists
 
 class CreateOrderService:
     def __init__(self, orders_repository: OrdersRepository):
         self.__orders_repository = orders_repository
     
     def create_order(self, data):
+        if data["restaurant_table_id"] is not None:
+            if not self.__orders_repository.check_table_exists(data):
+                raise TableNotExists(data['restaurant_table_id'])      
         return self.__orders_repository.create_order(data)
     
