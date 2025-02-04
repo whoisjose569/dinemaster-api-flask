21bf3f2ac0e6be1e9d9cce8eb2f5bfc84d513dc9
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-04 17:28:20 -0300

    chore: adjust presenter to show new field

--- src/presenters/orders/orders_presenter.py
+++ src/presenters/orders/orders_presenter.py
@@ -1,3 +1,4 @@
+import json
 class OrderPresenter:
     def format_order_presenter(self, data):
         return{
@@ -5,5 +6,9 @@ class OrderPresenter:
             "restaurant_table_id": data.restaurant_table_id,
             "create_at": data.create_at.strftime("%Y-%m-%d %H:%M:%S"),
             "order_status": data.order_status,
-            "delivery_type": data.delivery_type
+            "delivery_type": data.delivery_type,
+            "order_items": [
+                {"item": item["item"], "quantity": item["quantity"], "price": item["price"]}
+                for item in json.loads(data.order_items)  
+            ] if data.order_items else [] 
         }
