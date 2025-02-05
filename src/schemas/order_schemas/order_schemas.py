0a9bc5a1082b4301697f518bc7328da8d1051821
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-04 21:20:18 -0300

    feat: add custom errors to OrderSchema

--- src/schemas/order_schemas/order_schemas.py
+++ src/schemas/order_schemas/order_schemas.py
@@ -1,8 +1,28 @@
 from marshmallow import fields, validates
 from src.models.orders import Orders
-from src.errors.custom_errors import OrderStatusMustBeAString
+from src.errors.custom_errors import OrderStatusMustBeAString, OrderQuantityMustBeAInteger, OrderQuantityMustBeAGreaterThanZero, OrderPriceMustBeAFloat
 from src.utils import ma
 
+class OrderItemSchema(ma.Schema):
+    item = fields.Raw(required=True)
+    quantity = fields.Raw(required=True)
+    price = fields.Raw(required=True)
+    
+    @validates("quantity")
+    def validate_quantity(self, value):
+        if not isinstance(value, int):
+            raise OrderQuantityMustBeAInteger("Order quantity must be an Integer")
+    
+    @validates("quantity")
+    def validate_quantity_greater_than_zero(self, value):
+        if value <= 0:
+            raise OrderQuantityMustBeAGreaterThanZero("Quantity must be greater than zero")
+
+    @validates("price")
+    def validate_price_must_be_a_float(self, value):
+        if not isinstance(value, float):
+            raise OrderPriceMustBeAFloat("Price must be a Float")
+        
 class CreateOrderSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Orders
@@ -11,7 +31,7 @@ class CreateOrderSchema(ma.SQLAlchemyAutoSchema):
     restaurant_table_id = fields.Integer(load_default=None)
     order_status = fields.Raw(load_default="Em preparo")
     delivery_type = fields.String(load_default=None)
-    order_items = fields.List(fields.Dict(), load_default=[])
+    order_items = fields.List(fields.Nested(OrderItemSchema), load_default=[])
     
     @validates("order_status")
     def validate_order_status(self, value):
