446eb448ed4d02f850679cbf8718a5c4cc0590fc
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-10 15:03:27 -0300

    tests: implementing list orders test when not orders available

--- tests/unit/test_domain/test_orders_use_cases.py
+++ tests/unit/test_domain/test_orders_use_cases.py
@@ -1,6 +1,7 @@
 import pytest
 from src.domain.orders_use_cases.create_order import CreateOrderService
-from src.errors.custom_errors import TableNotExists
+from src.domain.orders_use_cases.list_orders import ListOrdersService
+from src.errors.custom_errors import TableNotExists, NotOrdersAvailable
 
 def test_create_order_when_table_not_exists(mocker):
     mock_repo = mocker.Mock()
@@ -13,3 +14,12 @@ def test_create_order_when_table_not_exists(mocker):
         service.create_order(data)
     
     mock_repo.check_table_exists.assert_called_once_with(data)
+
+def test_list_orders_not_orders_available(mocker):
+    mock_repo = mocker.Mock()
+    mock_repo.list_orders.return_value = False
+    
+    service = ListOrdersService(mock_repo)
+    
+    with pytest.raises(NotOrdersAvailable):
+        service.list_orders()
