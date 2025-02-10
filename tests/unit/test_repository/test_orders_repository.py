b5096d1d826bf7b3f2be6efbdd5cccf4b783095d
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-10 15:02:54 -0300

    tests: implementing list orders test repository

--- tests/unit/test_repository/test_orders_repository.py
+++ tests/unit/test_repository/test_orders_repository.py
@@ -1,3 +1,4 @@
+import json
 from src.models.orders import Orders
 from src.models.restaurant_table import RestaurantTable
 from src.repositories.orders.orders_repository import OrdersRepository
@@ -21,4 +22,34 @@ def test_orders_repository_create_order(db_session):
     db_session.commit()
     
     db_session.delete(result)
-    db_session.commit()  
+    db_session.commit()
+
+def test_orders_repository_list_orders(db_session):
+    restaurant_table = RestaurantTable(table_number = 125)
+    db_session.add(restaurant_table)
+    db_session.commit()
+
+    restaurant_table_on_db = db_session.query(RestaurantTable).filter_by(table_number=restaurant_table.table_number).first() 
+    
+    items = [
+        {"item": "X-Bacon", "quantity": 1, "price": 12.50},
+        {"item": "Coca-Cola Lata", "quantity": 2, "price": 5.00}
+    ]
+    
+    order = Orders(restaurant_table_id = restaurant_table_on_db.id, order_items = json.dumps(items))
+    db_session.add(order)
+    db_session.commit()
+    
+    order_on_db = db_session.query(Orders).filter_by(id=order.id).first()
+    order_on_db_items = json.loads(order_on_db.order_items)
+    
+    assert order_on_db.restaurant_table_id == restaurant_table_on_db.id
+    assert order_on_db_items == items
+    
+    db_session.delete(order_on_db)
+    db_session.commit()
+    
+    db_session.delete(restaurant_table_on_db)
+    db_session.commit()
+    
+    
