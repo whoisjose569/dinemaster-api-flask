14944ee8bde79612dfb44155243d019fa432fbb2
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-10 14:44:40 -0300

    feat: implementing list orders controller

--- src/controllers/orders/orders_controllers.py
+++ src/controllers/orders/orders_controllers.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from src.schemas.order_schemas.order_schemas import CreateOrderSchema
 from src.composer.orders_presenter_composer import order_presenter_composer
 from src.composer.orders.create_order_composer import create_order_composer
+from src.composer.orders.list_orders_composer import list_orders_composer
 
 bp = Blueprint('orders',__name__,url_prefix="/orders")
 
@@ -22,6 +23,17 @@ def create_order():
         return formatted_response, HTTPStatus.CREATED
     except Exception as exc:
         raise
-        
+
+@bp.route('/',methods=["GET"])
+def list_oders():
+    try:
+        use_case = list_orders_composer()
+        response = use_case.list_orders()
+
+        presenter = order_presenter_composer()
+        formatted_response = presenter.format_all_orders_presenter(response)
+        return formatted_response, HTTPStatus.OK
+    except Exception as exc:
+        raise         
         
         
