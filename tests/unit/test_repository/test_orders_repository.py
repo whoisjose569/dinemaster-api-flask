f8ad73ecb86c72a841ef82d76f755d57859840bd
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-10 17:17:24 -0300

    tests: implementing list order by id test not order available

--- tests/unit/test_repository/test_orders_repository.py
+++ tests/unit/test_repository/test_orders_repository.py
@@ -40,7 +40,39 @@ def test_orders_repository_list_orders(db_session):
     db_session.add(order)
     db_session.commit()
     
-    order_on_db = db_session.query(Orders).filter_by(id=order.id).first()
+    repo = OrdersRepository()
+    
+    order_on_db = repo.list_orders()
+    order_on_db_items = json.loads(order_on_db[0].order_items)
+    
+    assert order_on_db[0].restaurant_table_id == restaurant_table_on_db.id
+    assert order_on_db_items == items
+    
+    db_session.delete(order_on_db[0])
+    db_session.commit()
+    
+    db_session.delete(restaurant_table_on_db)
+    db_session.commit()
+
+def test_orders_repository_list_order(db_session):
+    restaurant_table = RestaurantTable(table_number = 125)
+    db_session.add(restaurant_table)
+    db_session.commit()
+
+    restaurant_table_on_db = db_session.query(RestaurantTable).filter_by(table_number=restaurant_table.table_number).first() 
+    
+    items = [
+        {"item": "X-Bacon", "quantity": 1, "price": 12.50},
+        {"item": "Coca-Cola Lata", "quantity": 2, "price": 5.00}
+    ]
+    
+    order = Orders(restaurant_table_id = restaurant_table_on_db.id, order_items = json.dumps(items))
+    db_session.add(order)
+    db_session.commit()
+    
+    repo = OrdersRepository()
+    
+    order_on_db = repo.list_order(order.id)
     order_on_db_items = json.loads(order_on_db.order_items)
     
     assert order_on_db.restaurant_table_id == restaurant_table_on_db.id
