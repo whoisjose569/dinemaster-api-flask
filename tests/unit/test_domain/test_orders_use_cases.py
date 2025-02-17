3360f54ae7f4f54cc7c3bb5ea08f281c146ba455
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-17 15:51:32 -0300

    tests: implementin tests for update_orders service

--- tests/unit/test_domain/test_orders_use_cases.py
+++ tests/unit/test_domain/test_orders_use_cases.py
@@ -2,6 +2,7 @@ import pytest
 from src.domain.orders_use_cases.create_order import CreateOrderService
 from src.domain.orders_use_cases.list_orders import ListOrdersService
 from src.domain.orders_use_cases.list_order import ListOrderService
+from src.domain.orders_use_cases.update_order import UpdateOrderStatusService
 from src.errors.custom_errors import TableNotExists, NotOrdersAvailable
 
 def test_create_order_when_table_not_exists(mocker):
@@ -33,3 +34,13 @@ def test_list_orders_not_order_available(mocker):
     
     with pytest.raises(NotOrdersAvailable):
         service.list_order(1)
+
+def test_update_order_not_order_available(mocker):
+    mock_repo = mocker.Mock()
+    mock_repo.check_order_exists.return_value = None
+    
+    service = UpdateOrderStatusService(mock_repo)
+    data = {}
+    
+    with pytest.raises(NotOrdersAvailable):
+        service.update_order(1, data)
